827ad18ab16057fbf5ad06fd6d429ec2e90b61d2
Andrea Fang <arhughes43@gmail.com>
2022-06-24 14:38:58 -0700

    dynamic display create-post and fix Post model

---
+++ public/javascript/add-post.js
@@ -0,0 +1,29 @@
+function newPostSection() {
+    var postSection = document.getElementById('create-post');
+    postSection.setAttribute('style', 'display: block')
+}
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_url = document.querySelector('input[name="post-url"]').value;
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_url
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if(response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+document.getElementById('new-post').addEventListener('click', newPostSection);
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler); 
